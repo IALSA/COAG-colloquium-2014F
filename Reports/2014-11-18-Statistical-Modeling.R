d863877c5ab8a16b40288596b90f2aa486f5813a
Andrey Koval <koval.andrey@gmail.com>
2014-11-14 10:34:02 -0800

    prep

--- Reports/2014-11-18-Statistical-Modeling.R
+++ Reports/2014-11-18-Statistical-Modeling.R
@@ -36,13 +36,8 @@ plotTheme <- ggplot2::theme_bw() +
   ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80"))+
   ggplot2::theme(axis.ticks.length = grid::unit(0, "cm"))
 
-## @knitr dsM01
+## @knitr dsM00
 dsM <- dplyr::filter(dsL, id == 1) %>% 
-  dplyr::select(id, year, attend, attendF)
-dsM
-
-## @knitr dsM02
-dsM <- dplyr::filter(dsL, id %in% c(1,23)) %>% 
   dplyr::select(id, year, attend, attendF)
 dsM
 
@@ -83,4 +78,258 @@ p <- p + labs(list(
  x="Years since 2000", y="Church attendance"))
 p <- p + facet_grid(sexF~raceF)
 p
+
+
+
+## @knitr dsM01
+dsM <- dplyr::filter(dsL, id == 1) %>%
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+dsM
+## @knitr graph01
+source("./Scripts/Graphs/basicTrajectory.R")
+p
+
+
+## @knitr dsM02
+dsM <- dplyr::filter(dsL, id == 2) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+dsM
+
+## @knitr graph02
+source("./Scripts/Graphs/basicTrajectory.R")
+p
+
+
+## @knitr dsM03
+dsM <- dplyr::filter(dsL, id == 3) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+dsM
+
+## @knitr graph03
+source("./Scripts/Graphs/basicTrajectory.R")
+p
+
+## @knitr dsM04
+dsM <- dplyr::filter(dsL, id == 4) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+dsM
+
+## @knitr graph04
+source("./Scripts/Graphs/basicTrajectory.R")
+p
+
+
+## @knitr graph05
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=0,y=2), size=7, shape="O", color="red")
+p
+
+## @knitr graph06
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=1,y=1), size=7, shape="O", color="red")
+p
+
+## @knitr graph07
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=2,y=3), size=7, shape="O", color="red")
+p
+
+## @knitr graph08
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=3,y=1), size=7, shape="O", color="red")
+p
+
+## @knitr graph09
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=3,y=2), size=7, shape="O", color="red")
+p
+
+## @knitr graph10
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=2,y=1), size=7, shape="O", color="red")
+p
+
+## @knitr graph11
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_point(aes(x=1,y=6), size=7, shape="O", color="red")
+p
+
+
+## @knitr dsM12
+dsM <- dplyr::filter(dsL, id == 1) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+dsM$model <- 4
+dsM
+## @knitr graph12
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+## @knitr dsM13
+dsM <- dplyr::filter(dsL, id == 1) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 4 - dsM$time
+dsM
+## @knitr graph13
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+## @knitr dsM14
+dsM <- dplyr::filter(dsL, id == 1) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 4 - (.2)*dsM$time
+dsM
+## @knitr graph14
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+
+## @knitr dsM15
+dsM <- dplyr::filter(dsL, id == 2) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 1.8 + (.05)*dsM$time
+dsM
+## @knitr graph15
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+
+## @knitr dsM16
+dsM <- dplyr::filter(dsL, id == 3) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 2 + (.17)*dsM$time
+dsM
+## @knitr graph16
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+
+## @knitr dsM17
+dsM <- dplyr::filter(dsL, id == 4) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 1.5 + (.22)*dsM$time
+dsM
+## @knitr graph17
+source("./Scripts/Graphs/basicTrajectory.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+## @knitr dsM18
+dsM <- dplyr::filter(dsL, id <= 4) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 3 - (.14)*dsM$time
+dplyr::filter(dsM,id==1)
+## @knitr graph18
+p <- ggplot2::ggplot(dsM,aes(x=time,y=attend))
+p <- p + geom_line(aes(group=id), color='firebrick',
+                   alpha=.2,
+                   position=position_jitter(w=0.1, h=0.1))
+p <- p + geom_point(shape=1, color="black", fill=NA,                 
+                    alpha=.4, size=2, 
+                    position=position_jitter(w=0.1, h=0.1))
+p <- p + plotTheme
+p <- p + scale_x_continuous(limits=c(-.5,11),
+                            breaks=c(0:11))
+p <- p + scale_y_continuous(limits=c(-.5,8.5), 
+                            breaks=seq(1,8, by=1))
+p <- p + labs(list(
+  title="How often did you attend worship last year?",
+  x="Time scale: years since 2000", y="Church attendance"))
+#
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.5, alpha=.3)
+p
+
+
+## @knitr dsM19
+dsM <- dplyr::filter(dsL, id <= 300) %>% 
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- 3 - (.14)*dsM$time
+dplyr::filter(dsM,id==1)
+## @knitr graph19
+source("./Scripts/Graphs/basicTrajectory300.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.7, alpha=.5)
+p
+
+## @knitr dsM20
+dsM <- dplyr::filter(dsL, id <= 300) %>% 
+  dplyr::filter(ave((!is.na(attend)), id, FUN = all)) %>%
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- predict (lm(attend ~ 1, data=dsM))
+dplyr::filter(dsM,id==1)
+## @knitr graph20
+source("./Scripts/Graphs/basicTrajectory300.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.7, alpha=.5)
+p
+
+
+## @knitr dsM21
+dsM <- dplyr::filter(dsL, id <= 300) %>% 
+  dplyr::filter(ave((!is.na(attend)), id, FUN = all)) %>%
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend) 
+dsM$model <- predict (lm(attend ~ 1 + time, data=dsM))
+dplyr::filter(dsM,id==1)
+## @knitr graph21
+source("./Scripts/Graphs/basicTrajectory300.R")
+p <- p + geom_line(aes(y=model), color="blue4", linetype="solid", size=1.7, alpha=.5)
+p
+
+
+## @knitr dsM22
+dsM <- dplyr::filter(dsL, id <= 300) %>% 
+  dplyr::filter(ave((!is.na(attend)), id, FUN = all)) %>%
+  dplyr::mutate(time=year-2000) %>%
+  dplyr::select(id, time, attend)
+modelA <- lm(attend ~ 1, data=dsM)
+modelB <- lm(attend ~ 1 + time, data=dsM)
+dplyr::filter(dsM,id==1)
+
+## @knitr model0
+summary(modelA)
+
+
+#
+#
+summary(modelB)
+
+
+
+## @knitr model1
+deviance(modelA)
+BIC(modelA)
+AIC(modelA)
+
+
+
+#
+#
+
+
+deviance(modelB)
+BIC(modelB)
+AIC(modelB)
+
 
